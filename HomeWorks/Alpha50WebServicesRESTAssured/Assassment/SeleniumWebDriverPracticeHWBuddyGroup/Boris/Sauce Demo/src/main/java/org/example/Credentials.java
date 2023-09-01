a65cea90d85cd78417fe8c2e8f52e421447cc771
FGargov <fgargov@gmail.com>
2023-09-01 09:35:20 +0300

    All changes

--- HomeWorks/Alpha50WebServicesRESTAssured/Assassment/SeleniumWebDriverPracticeHWBuddyGroup/Boris/Sauce Demo/src/main/java/org/example/Credentials.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Credentials {
-
-    public static final String VALID_USERNAME = "standard_user";
-    public static final String VALID_PASSWORD = "secret_sauce";
-
-}
--- HomeWorks/Alpha50WebServicesRESTAssured/Assassment/SeleniumWebDriverPracticeHWBuddyGroup/Boris/Sauce Demo/src/main/java/org/example/WebElementsIDs.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class WebElementsIDs {
-    public static final String FIRST_NAME_BOX_ID = "first-name";
-    public static final String LAST_NAME_BOX_ID = "last-name";
-    public static final String POSTAL_CODE_BOX_ID = "postal-code";
-}
